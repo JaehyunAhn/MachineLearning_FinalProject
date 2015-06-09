7da76a35e4e1be16cc19e20b9eae5043c3d437ec
= <jaehyunahn@sogang.ac.kr>
2015-06-09 10:15:06 +0900

    SVC added

--- PredicitonModule.py
+++ PredicitonModule.py
@@ -3,9 +3,8 @@
     - Author : Jaehyun Ahn (jaehyunahn@sogang.ac.kr)
     - Due Date : 15/06/16
 """
-
 from scipy.io import loadmat
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVC, SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from DataModule import *
@@ -16,6 +15,7 @@ from DataModule import *
     - objectB : B label, dataset # 16
     - test_set : test set, which is unlabeled # 48
 """
+print("Reading dataset from matlab...")
 objectA = loadmat('./datang/objectA.mat')
 objectB = loadmat('./datang/objectB.mat')
 test_set = loadmat('./datang/test_set.mat')
@@ -23,6 +23,7 @@ test_set = loadmat('./datang/test_set.mat')
 # print(objectA['objectA'])
 # print(objectB['objectB'])
 # print(test_set['test_set'])
+print("Data loaded.")
 
 image_arr = []
 image_arr2 = []
@@ -31,6 +32,7 @@ label_index2 = []
 data_list = {'label': label_index, 'data': image_arr}
 test_list = {'label': label_index2, 'data': image_arr2}
 
+print("Collect training/testing dataset..\n")
 train = data_list
 train = collect_data(object_dataset=objectA['objectA'],
                      label_name=0,
@@ -47,29 +49,48 @@ train_data = np.vstack(train['data'])
 train_label = np.hstack(train['label'])
 test_data = np.vstack(test['data'])
 
+print("-TEST # DATASET: %d" % len(test_set['test_set']))
+
 # Random Forest
+print("RANDOM FOREST CLASSIFIER")
 clf_rf = RandomForestClassifier()
 clf_rf.fit(train_data, train_label)
 y_pred_rf = clf_rf.predict(test_data)
-print("Random Forest Array: ", y_pred_rf)
+print("-Random Forest Pred:\t", y_pred_rf)
+print("\n")
 
 # Linear SVM
+print("LINEAR SUPPORT VECTOR MACHINE")
 clf_svm = LinearSVC()
 clf_svm.fit(train_data, train_label)
 y_pred_svm = clf_svm.predict(test_data)
-print("LSVM Array: ", y_pred_rf)
+print("-LSVM Pred:\t\t\t", y_pred_rf)
+print("\n")
+
+# Support Vector Classifier
+print("SUPPORT VECTOR CLASSIFIER")
+clf_svc = SVC()
+clf_svc.fit(train_data, train_label)
+y_pred_svc = clf_svc.predict(test_data)
+print("-SVC Pred:\t\t\t", y_pred_svc)
+print("\n")
 
 # KNN Classifier
+print("K-NEAREST NEIGHBOR CLASSIFIER")
 clf_knn = KNeighborsClassifier()
 clf_knn.fit(train_data, train_label)
 y_pred_knn = clf_knn.predict(test_data)
-print("KNN Array: ", y_pred_knn)
+print("-KNN Pred:\t\t\t", y_pred_knn)
+print("\n")
 
+print("Learning is done.")
 """
-RF, LSVM, KNN
+Unscaled
+RF, LSVM, SVC, KNN
 [0 1 1 1 1 0 0 0 0 1 1 1 1 0 1 1 0 0 0 0 1 1 0 0 0 1 0 0 0 1 1 1 0 0 1 0 0 1 1 1 0 1 0 1 1 1 0 0]
 [0 1 1 1 1 0 0 0 0 1 1 1 1 0 1 1 0 0 0 0 1 1 0 0 0 1 0 0 0 1 1 1 0 0 1 0 0 1 1 1 0 1 0 1 1 1 0 0]
 [0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 1 0 0 0 0 1 1 0 0 0 0 0 0 0 1 1 0 0 0 1 0 0 1 0 0 0 1 0 1 1 1 0 0]
+[1 1 1 1 1 0 0 0 0 1 1 1 0 0 1 1 1 1 1 0 1 1 1 1 1 1 0 0 0 1 1 1 1 1 1 0 0 1 1 1 0 1 0 1 1 1 0 0]
 PREDICTION
 [0 1 1 1 1 0 0 0 0 1 1 1 1 0 1 1 0 0 0 0 1 1 0 0 0 1 0 0 0 1 1 1 0 0 1 0 0 1 1 1 0 1 0 1 1 1 0 0]
 """
